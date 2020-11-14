53b6415da9542b4313cac431e0738d194cfe4735
zGuil <guilherme.faraujo@aluno.faculdadeimpacta.com.br>
2020-11-14 01:44:24 -0300

    Subindo new Front

--- server/routes/health.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-
-bp = Blueprint("health", __name__)
-
-
-@bp.route("/health", methods=["GET"])
-def get_health_route():
-    return "<h1>To Funcionando </h1>", 200
--- web/frontend-styled-components/src/pages/Home/index.tsx
+++ web/frontend-styled-components/src/pages/Home/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
-import { FcCurrencyExchange, FcFolder, FcOpenedFolder } from "react-icons/fc";
+import { FcMoneyTransfer } from "react-icons/fc";
+import { HiFolderAdd } from "react-icons/hi";
+import { RiFileSearchFill } from "react-icons/ri";
+import { BsBarChartFill } from "react-icons/bs";
 import { Link } from "react-router-dom"
+
+import Logo from "../../assets/logo.png"
 
 import './index.css'
 
@@ -8,31 +13,44 @@ import './index.css'
 function Home() {
 
     return (
+        <>
+        <header className="logo-main">
+            <img src={Logo} alt="logo"/>
+        </header>
+
         <div className="field-group">
             <div className="field">
                 <Link to="/vendas">
-                    <FcCurrencyExchange size={250}/>
+                    <FcMoneyTransfer size={250} color="#a0ba49"/>
                     <span>Efetuar Vendas</span>
                 </Link>
             </div>
 
             <div className="field">
             <Link to="/cadastro-produto">
-                <FcFolder size={250} />
+                <HiFolderAdd size={250} color="#a0ba49"/>
                 <span>Cadastrar Produto</span>
             </Link>   
             </div>
 
             <div className="field">
+            <Link to="/relatorio-vendas">
+                <BsBarChartFill size={250} color="#a0ba49"/>
+                <span>Relatórios de Vendas</span>
+            </Link>   
+            </div>
+
+            <div className="field">
                 <Link to="/estoque">
-                    <FcOpenedFolder size={250} />
-                    <span>Verificar Estoque Atual</span>
+                    <RiFileSearchFill size={250} color="#a0ba49"/>
+                    <span>Verificar Estoque</span>
                 </Link>
                 
             </div>
 
         </div>
         
+        </>
     )
   }
   
